8804a7d0494ccb74ca7cdd359fdca14af98c8839
Lucas Pontes <75232236+Bridges369@users.noreply.github.com>
2022-05-28 14:50:20 -0300

    fixing

--- mapping.ps1
+++ mapping.ps1
@@ -1,30 +1,15 @@
-New-Item ` # git
-  -Path "~\.gitconfig" `
-  -ItemType SymbolicLink `
-  -Target "~\.dotfiles\.gitconfig" `
-  -Force
+# git
+New-Item -Path "~\.gitconfig" -ItemType SymbolicLink -Target "~\.dotfiles\.gitconfig" -Force
 
-New-Item ` # powershell
-  -Path "$env:CURRENTUSER\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" `
-  -ItemType SymbolicLink `
-  -Target "~\.dotfiles\.profile.json" `
-  -Force
+# powershell
+New-Item -Path "$env:CURRENTUSER\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Target "~\.dotfiles\.profile.ps1" -Force
 
-New-Item ` # windows terminal
-  -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" `
-  -ItemType SymbolicLink `
-  -Target "~\.dotfiles\.wterm.json" `
-  -Force
+# windows terminal
+New-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -ItemType SymbolicLink -Target "~\.dotfiles\.wterm.json" -Force
 
-New-Item ` # neovim
-  -Path "$env:LOCALAPPDATA\nvim\init.vim" `
-  -ItemType SymbolicLink
-  -Target "~\.dotfiles\.vim" `
-  -Force
+# neovim
+New-Item -Path "$env:LOCALAPPDATA\nvim\init.vim" -ItemType SymbolicLink -Target "~\.dotfiles\.vim" -Force
 
-New-Item ` # oh-my-posh
-  -Path "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\my_schema1.json" `
-  -ItemType SymbolicLink `
-  -Target "~\.dotfiles\.posh.json" `
-  -Force
+# oh-my-posh
+New-Item -Path "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\my_schema1.json" -ItemType SymbolicLink -Target "~\.dotfiles\.posh.json" -Force
 
