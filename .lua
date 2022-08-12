b345c87c9edf3fd03a95a527631f7ebab26133a6
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2022-08-12 01:09:15 -0300

    created lua main file

---
+++ .lua
@@ -0,0 +1,10 @@
+vim.cmd([[
+source $HOME/.dotfiles/lua-src/call-plugs.lua
+source $HOME/.dotfiles/lua-src/global-sets.lua
+source $HOME/.dotfiles/lua-src/variables.lua
+source $HOME/.dotfiles/lua-src/functions.lua
+source $HOME/.dotfiles/lua-src/remaps.lua
+source $HOME/.dotfiles/lua-src/autocmd.lua
+source $HOME/.dotfiles/lua-src/command.lua
+source $HOME/.dotfiles/lua-src/theme.lua
+]])
