5b621a49dae606a63a499c26dfd68c5ac7a489ca
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2022-08-12 01:11:53 -0300

    change symbolic link of init.vim to init.lua

--- ambient/mapping.ps1
+++ ambient/mapping.ps1
@@ -8,7 +8,7 @@ New-Item -Path "C:$env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profil
 New-Item -Path "C:$env:HOMEPATH\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\.wterm.json" -Force
 
 # neovim
-New-Item -Path "C:$env:HOMEPATH\AppData\Local\nvim\init.vim" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\.vim" -Force
+New-Item -Path "C:$env:HOMEPATH\AppData\Local\nvim\init.lua" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\.lua" -Force
 
 # oh-my-posh
 New-Item -Path "C:$env:HOMEPATH\Programs\oh-my-posh\themes\my_schema1.json" -ItemType SymbolicLink -Target "C:$env:HOMEPATH\.dotfiles\.posh.json" -Force
