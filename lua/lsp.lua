c047a7724ad8b4f44a632789bd988352c999160f
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2024-02-17 15:47:20 -0300

    assoc 'mcmeta' to 'jsonc'

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,10 +1,18 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require("lspconfig").sumneko_lua.setup {}
+require("lspconfig").lua_ls.setup {}
 require("lspconfig").solargraph.setup {}
 require("lspconfig").jsonls.setup {
   capabilities = capabilities,
   filetypes = { "json", "jsonc" },
 }
 
+local group = vim.api.nvim_create_augroup("json_files", {clear = true})
+-- JSON
+vim.api.nvim_create_autocmd(
+  { "BufEnter", "BufNew", "BufWinEnter" }, {
+    group = "json_files",
+    pattern = { "*.mcmeta", "*.json" },
+    command = "set filetype=jsonc",
+  })
