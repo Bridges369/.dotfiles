eff875b739ab754677d4eede29be0663e50b123c
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2025-12-04 16:15:02 -0300

    .

---
+++ r/demo.R
@@ -0,0 +1,21 @@
+require(pacman)
+library(pacman)
+
+pacman::p_load(pacman, 
+               dplyr, # a Grammar of Data Manipulation
+               GGally, # 
+               ggplot2, # Create Elegant Data Visualisations Using the Grammar of Graphics
+               ggthemes, #
+               ggvis, #
+               httr, #
+               lubridate, #
+               plotly, #
+               rio, #
+               rmarkdown, # Dynamic Documents for R
+               shiny, #
+               stringr, # Simple, Consistent, Wrappers for Common String Operations
+               tidyr, #
+               accelerometry #
+        )
+
+#.
